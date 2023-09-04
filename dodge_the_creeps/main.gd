69ac9b73d8cd2009a2b9ae7ab7ed7a537308b1cb
airak <matheus.mmag@gmail.com>
2023-09-04 18:42:17 -0300

    heads up display

--- dodge_the_creeps/main.gd
+++ dodge_the_creeps/main.gd
@@ -16,11 +16,15 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start() # Replace with function body.
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 
 func _on_mob_timer_timeout():
@@ -51,6 +55,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
